4f6816310281e4d786fc8d945a113c89532f91c1
root <root@anton-v.zo-intern.de>
2017-07-07 08:52:35 +0200

    Detect namespace-events

--- action.php
+++ action.php
@@ -18,40 +18,89 @@ class action_plugin_filespace extends DokuWiki_Action_Plugin {
      * @return void
      */
     public function register(Doku_Event_Handler $controller) {
-       $controller->register_hook('IO_NAMESPACE_CREATED', 'AFTER', $this, 'handle_io_namespace_created');
-       $controller->register_hook('IO_NAMESPACE_DELETED', 'AFTER', $this, 'handle_io_namespace_deleted');
        $controller->register_hook('IO_WIKIPAGE_WRITE', 'BEFORE', $this, 'handle_io_wikipage_write');
     }
 
     /**
-     * [Custom event handler which performs action]
+     * IO_WIKIPAGE_WRITE/BEFORE
      *
      * @param Doku_Event $event  event object by reference
      * @param mixed      $param  [the parameters passed as fifth argument to register_hook() when this
      *                           handler was registered]
      * @return void
      */
+    public function handle_io_wikipage_write(Doku_Event &$event, $param)
+    {
+        global $conf;
+        global $INFO;
 
-    public function handle_io_namespace_created(Doku_Event &$event, $param) {
-		global $conf;
-		if ($data[1] != 'pages') return;
-		$ns = $data[0];
-		$t_dirname = "";
-		if ($conf['useheading']) {
-			$t_dirname = p_get_first_heading($ns + $conf['start']);
-		} else {
-			$t_dirname = curNS($ns);
-		}
-		dbg("Erstelle Ordner: ".$t_dirname);
-    }
+if ($INFO['namespace'] != 'home:ogeisen:new_namespace_test_for_plugin') return;
 
-    public function handle_io_namespace_deleted(Doku_Event &$event, $param) {
-		global $conf;
-    }
+        $page = $event->data[2];
+        $ns = $event->data[1];
+        $is_attic = $event->data[3];
 
-    public function handle_io_wikipage_write(Doku_Event &$event, $param) {
-		global $conf;
-    }
+        if ($is_attic) return;  // don't handle archived pages
+        if ($page != $conf['start']) return;  // only handle namespace pages
+        if ($ns == '') return;  // void wiki startpage
+
+        if ( ! $INFO['exists']) {
+            $new_title = '';
+            if (preg_match('/^==+(.+?)==+/', $event->data[0][1], $match)) {
+                $new_title = trim($match[1]);
+            }
+msg("New namespace page created. Try to create filesystem dir '$new_title' too.");
+            #_page_created();
+        }
+        elseif ($event->data[0][1] == '') {
+            $id = $ns . ':' . $page;
+            $old_title = p_get_first_heading($id);
+msg("Namespace page deleted. Try to delete filesystem dir '$old_title' too.");
+        }
+        else {
+msg("Namespace page edited. Check if Title has changed");
+            $id = $ns . ':' . $page;
+            $old_title = p_get_first_heading($id);
+msg("old_title: $old_title");
+
+            $new_title = '';
+            if (preg_match('/^==+(.+?)==+/', $event->data[0][1], $match)) {
+                $new_title = trim($match[1]);
+            }
+msg("new_title: $new_title");
+
+            if ($new_title != $old_title) {
+msg("Namespace page title has changed from '$old_title' -> '$new_title'. Try to change filesystem dir too.");
+            } else {
+msg("NO FILESPACE ACTION REQUIRED :-)");
+            }
+        }
+
+
+#msg("<pre>\$INFO:\n".print_r($INFO,true)."</pre>");
+// if there is no previous version of the current page, it must be the initial one
+// if the 
+        
+/*
+        $exist = $INFO['exists'];
+
+            if (( ! $rev) && ( ! $exist) && ($name == $conf['start'])) {
+                msg("NAMESPACE STARTPAGE CREATED");
+            }
+            elseif (
+            #msg("<pre>IO_WIKIPAGE_WRITE\n".print_r($event,true).print_r($INFO,true)."</pre>",0);
+msg("Namespace: $ns");
+    msg("<pre>IO_NAMESPACE_DELETED\n".print_r($event,true)."</pre>",0);
+        $t_dirname = "";
+        if ($conf['useheading']) {
+            $t_dirname = p_get_first_heading($ns . $conf['start']);
+        } else {
+            $t_dirname = curNS($ns);
+        }
+msg("Erstelle Ordner: ".$t_dirname);
+msg("Erstelle Ordner: ".curNS($ns));
+*/
+     }
 
 }
 
